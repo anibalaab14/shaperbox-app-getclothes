a7149ae68f74f8bf86399ea47b4ef8099a810acc
Anibal Alfaro B <anibal.aab.91@gmail.com>
2021-04-01 03:01:19 -0300

    primer commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # shaperbox-app-getclothes
+
+# AFTER CLONING REPOSITORY
+
+npm install
+npm start
+
+# ENVIROMENT VARIABLES
+
+- PORT
+- CLOUD_NAME_CLOUDINARY
+- API_KEY_CLOUDINARY
+- API_SECRET_CLOUDINARY
+
+# FOR DEPLOY
+
+for deploy in production:
+- git push heroku 
+
+# FOR PUSH 
+
+for pushit github branch
+- git push 
---
+++ index.js
@@ -0,0 +1,42 @@
+var cloudinary = require('cloudinary');
+require('dotenv').config();
+const express = require('express');
+const app = express()
+app.use(express.json());
+
+cloudinary.v2.config({
+    cloud_name: process.env.CLOUD_NAME_CLOUDINARY,
+    api_key: process.env.API_KEY_CLOUDINARY,
+    api_secret: process.env.API_SECRET_CLOUDINARY
+});
+
+
+
+app.post('/shaperbox/v1/app-getclothes', function (req, res) {
+
+    
+    if (req.body.screen=="front"){
+    console.log(req.body.quantity);
+    cloudinary.v2.search
+        .expression('resource_type:image AND')
+        .sort_by('public_id', 'desc')
+        .max_results(req.body.quantity)
+        .execute().then(result => {
+            res.send(result);
+        });  
+             //Se debe agregar para que busque 
+            //imagenes de pantalla informativa sobre como utilizar laapp
+    }else{
+        console.log(req.body.quantity);
+        cloudinary.v2.search
+            .expression('resource_type:image')
+            .sort_by('public_id', 'desc')
+            .max_results(req.body.quantity)
+            .execute().then(result => {
+                res.send(result);
+            });
+    }
+
+});
+app.listen(process.env.PORT);
+console.log(process.env.PORT);
